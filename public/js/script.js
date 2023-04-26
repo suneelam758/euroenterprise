7adbbabd67e963f20716f2704195e41ba3b468b7
Suneelam <suneelamtripathi123456@gmail.com>
2023-04-27 00:51:11 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -41,7 +41,7 @@
 			pageTransition({
 				target: document.querySelector( '.page' ),
 				delay: 0,
-				duration: 500,
+				duration: 100,
 				classIn: 'fadeIn',
 				classOut: 'fadeOut',
 				classActive: 'animated',
